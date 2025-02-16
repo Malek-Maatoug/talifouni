79c3a9cb6a885dbc48269371116e13952b31dca2
ibtihelbs <ibtihel.bensalah@outlook.fr>
2025-02-16 10:54:39 +0100

    text component

---
+++ app/components/core/Text.jsx
@@ -0,0 +1,21 @@
+const Text = ({ content, className }) => {
+  return <p className={` text-xl ${className}`}> {content} </p>;
+};
+
+const Title = ({ content, className }) => {
+  return (
+    <h1 className={`font-[family-name:var(--title) ${className}`}>
+      {content}{" "}
+    </h1>
+  );
+};
+
+const SubTitle = ({ content, className }) => {
+  return (
+    <h3 className={`font-[family-name:var(--title) ${className}`}>
+      {" "}
+      {content}{" "}
+    </h3>
+  );
+};
+export { Text, Title, SubTitle };
