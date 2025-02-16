cd745be74694e56d64e36abd0656d840b2a433c2
ibtihelbs <ibtihel.bensalah@outlook.fr>
2025-02-16 11:49:58 +0100

    layout

---
+++ app/components/core/Layouts.jsx
@@ -0,0 +1,5 @@
+const Centered = ({ children }) => {
+  return <div className="grid justify-items-center gap-6 ">{children}</div>;
+};
+
+export { Centered };
--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,18 @@
+import { Centered } from "./components/core/Layouts";
 import { Text, Title } from "./components/core/Text";
 
 export default function Home() {
   return (
     <>
-      <section>
+      <Centered>
         <Title content={"hefheoigehfpoejgpeogjpo"} />
         <Text
-          className={"bg-red-300 "}
+          className={"text-center max-w-96"}
           content={
             " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, ut molestias nulla accusantium perferendis a? Ex perferendis ipsa totam, architecto dolor nobis cupiditate dolores, optio quam enim adipisci error maxime."
           }
         />
-      </section>
+      </Centered>
     </>
   );
 }
