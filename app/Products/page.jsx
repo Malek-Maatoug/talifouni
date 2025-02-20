1e46cdfa58a60bd69c40e095e72096a8affc1f47
Malek Maatoug <maatougmalek46@gmail.com>
2025-02-20 18:21:21 +0100

    Page All Products commit

--- app/Products/page.jsx
+++ app/Products/page.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Image from 'next/image';
+import SingleProduct from '../components/core/SingleProduct';
 
 const page = () => {
+    const [phones, setPhones] = useState([]);
+    const getPhones = async ()=> {
+        try {
+            const res = await axios.get("http://localhost:3000/phones");
+            setPhones(res.data); // Access the "phones" array from the response
+        } catch (error) {
+            console.error("Error fetching phones:", error);
+        }
+    }
+    useEffect(()=>{
+        getPhones()
+    },[])
+    console.log(phones);
+    
+    
+    
+    
   return (
-    <div>page</div>
+    <div className='min-w-full min-h-full grid grid-cols-2 gap-5'>
+        {phones ? (
+          phones.map((v) => (
+            <SingleProduct
+              key={v.id}
+              phone={v}
+            />
+          ))
+        ) : (
+          <h1>Loadinng ......</h1>
+        )}
+    </div>
   )
 }
 
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,6 +10,15 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        cyan: {
+          strong: "hsl(172, 67%, 45%)",
+          "very-dark": "hsl(183, 100%, 15%)",
+          "dark-grayish": "hsl(186, 14%, 43%)",
+          grayish: "hsl(184, 14%, 56%)",
+          "light-grayish": "hsl(185, 41%, 84%)",
+          "very-light-grayish": "hsl(189, 41%, 97%)",
+        },
+        white: "hsl(0, 0%, 100%)",
       },
     },
   },
