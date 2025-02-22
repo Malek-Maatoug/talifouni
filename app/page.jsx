332285d8c2946cc3e77fdd24c6255a2db37ddaec
unknown <najwaislem125@gmail.com>
2025-02-22 09:07:49 +0100

    lol

--- app/page.jsx
+++ app/page.jsx
@@ -5,11 +5,11 @@ export default function Home() {
   return (
     <>
       <Centered>
-        <Title content={"hefheoigehfpoejgpeogjpo"} />
+        <Title content={"oooooooooooooo"} />
         <Text
           className={"text-center max-w-96"}
           content={
-            " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, ut molestias nulla accusantium perferendis a? Ex perferendis ipsa totam, architecto dolor nobis cupiditate dolores, optio quam enim adipisci error maxime."
+            " helloooooo"
           }
         />
       </Centered>
