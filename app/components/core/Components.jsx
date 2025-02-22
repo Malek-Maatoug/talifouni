73d3331833b917c47d8d0c9f1f40d12a8f86f07a
unknown <najwaislem125@gmail.com>
2025-02-22 11:30:31 +0100

    single page edited

---
+++ app/components/core/Components.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+const LiensBtn = ({ content, link }) => {
+  return (
+    <Link className="px-2 py-1 rounded-full bg-blue-200" href={link}>
+      {content}
+    </Link>
+  );
+};
+
+export { LiensBtn };
